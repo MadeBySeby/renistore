375cc42f9c31d9afaca3b3ec3a070d7d26b3001e
dato sebiskveradze <david.katsitadze.1@btu.edu.ge>
2025-12-21 17:20:49 +0400

    refactor: remove unused import from products route

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,5 +1,4 @@
 import { createClient } from "@/lib/supabase/server";
-import { useEffect } from "react";
 // solution number 2 if there is volnobility with server ts
 export async function Get() {
   const supabase = await createClient();
